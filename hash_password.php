b83dc76e4e70fc363c4428ea36048de251234669
Prakriti Kunwar <kunwarprakreety@gmail.com>
2025-08-27 15:47:45 +0545

    Stop tracking hash_password.php

--- hash_password.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Replace "newpassword123" with the password you want for admin
-$new_password = "admin123"; 
-$hashed = password_hash($new_password, PASSWORD_DEFAULT);
-echo "Hashed password: " . $hashed;
-?>
